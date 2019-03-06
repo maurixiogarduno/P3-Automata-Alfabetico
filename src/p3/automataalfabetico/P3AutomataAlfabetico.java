63706a56028468479a689da492b2eed481b14b38
Mauricio Garduño <mau.gar.gonz@icloud.com>
2019-03-06 01:54:36 -0600

    hola bb

---
+++ src/p3/automataalfabetico/P3AutomataAlfabetico.java
@@ -0,0 +1,20 @@
+package p3.automataalfabetico;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Jedidian
+ */
+public class P3AutomataAlfabetico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main inicio = new Main();
+        inicio.setVisible(true);
+    }
+    
+}
